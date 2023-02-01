d066eb3c8e0f472576e5e2406366d6d0392069b4
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-02-01 13:37:07 -0500

    updated images to https

--- utils/property/property.js
+++ utils/property/property.js
@@ -1,11 +1,11 @@
 export function getImages(property) {
   let imgs = [];
   const mainUrl = {
-    url: `http:${property.mainPhoto.fields.file.url}`,
+    url: `https:${property.mainPhoto.fields.file.url}`,
     alt: property.mainPhoto.fields.description,
   };
   const addUrls = property.additionalPhotos.map((photo) => ({
-    url: `http:${photo.fields.file.url}`,
+    url: `https:${photo.fields.file.url}`,
     alt: photo.fields.description,
   }));
   imgs = [mainUrl, ...addUrls];
